87e5e48de7766ed01e4899382dac935f68db94ef
Alexis Tessier <alexistessierdev@gmail.com>
2017-06-13 22:32:51 +0200

    end file-writer tests rewriting

--- tests/specs/file-writer.test.js
+++ tests/specs/file-writer.test.js
@@ -155,6 +155,346 @@ const writeToPromiseAndCallbackStyleMacro = [writeToPromiseStyleMacro, writeToCa
 /*-------------------------------------------------*/
 /*-------------------------------------------------*/
 
+const writerPossibilities = [
+	{ write: 'string' },
+	{ write: 'buffer' },
+	{ write: 'stream' },
+	{ write: 'directory' },
+	{ write: 'promise resolving string' },
+	{ write: 'promise resolving buffer' },
+	{ write: 'promise resolving stream' },
+	{ write: 'promise resolving directory' },
+	{ write: 'promise resolving function resolving string' },
+	{ write: 'promise resolving function resolving buffer' },
+	{ write: 'promise resolving function resolving stream' },
+	{ write: 'promise resolving function resolving directory' },
+	{ write: 'promise resolving promise resolving string' },
+	{ write: 'promise resolving promise resolving buffer' },
+	{ write: 'promise resolving promise resolving stream' },
+	{ write: 'promise resolving promise resolving directory' },
+	{ write: 'promise resolving promise resolving function resolving string' },
+	{ write: 'promise resolving promise resolving function resolving buffer' },
+	{ write: 'promise resolving promise resolving function resolving stream' },
+	{ write: 'promise resolving promise resolving function resolving directory' },
+	{ write: 'function resolving string' },
+	{ write: 'function resolving buffer' },
+	{ write: 'function resolving stream' },
+	{ write: 'function resolving directory' },
+	{ write: 'function resolving promise resolving string' },
+	{ write: 'function resolving promise resolving buffer' },
+	{ write: 'function resolving promise resolving stream' },
+	{ write: 'function resolving promise resolving directory' },
+	{ write: 'function resolving function resolving string' },
+	{ write: 'function resolving function resolving buffer' },
+	{ write: 'function resolving function resolving stream' },
+	{ write: 'function resolving function resolving directory' },
+	{ write: 'function resolving promise resolving function resolving string' },
+	{ write: 'function resolving promise resolving function resolving buffer' },
+	{ write: 'function resolving promise resolving function resolving stream' },
+	{ write: 'function resolving promise resolving function resolving directory' },
+	{ copy: 'absolute path' },
+	{ copy: 'relative path' },
+	{ copy: 'absolute directory' },
+	{ copy: 'relative directory' },
+	{ copy: 'promise resolving absolute path' },
+	{ copy: 'promise resolving relative path' },
+	{ copy: 'promise resolving absolute directory' },
+	{ copy: 'promise resolving relative directory' },
+	{ copy: 'promise resolving function resolving absolute path' },
+	{ copy: 'promise resolving function resolving relative path' },
+	{ copy: 'promise resolving function resolving absolute directory' },
+	{ copy: 'promise resolving function resolving relative directory' },
+	{ copy: 'promise resolving promise resolving absolute path' },
+	{ copy: 'promise resolving promise resolving relative path' },
+	{ copy: 'promise resolving promise resolving absolute directory' },
+	{ copy: 'promise resolving promise resolving relative directory' },
+	{ copy: 'function resolving absolute path' },
+	{ copy: 'function resolving relative path' },
+	{ copy: 'function resolving absolute directory' },
+	{ copy: 'function resolving relative directory' },
+	{ copy: 'function resolving promise resolving absolute path' },
+	{ copy: 'function resolving promise resolving relative path' },
+	{ copy: 'function resolving promise resolving absolute directory' },
+	{ copy: 'function resolving promise resolving relative directory' },
+	{ copy: 'function resolving function resolving absolute path' },
+	{ copy: 'function resolving function resolving relative path' },
+	{ copy: 'function resolving function resolving absolute directory' },
+	{ copy: 'function resolving function resolving relative directory' }
+];
+
+writerPossibilities.forEach(possibility => {
+	function p(...args) {
+		return Object.assign({}, possibility, ...args)
+	}
+
+	test.cb(writeToPromiseAndCallbackStyleMacro, p({
+		expectFileWasCreated: true
+	}));
+
+	test.cb('writeTo a non existent path', writeToPromiseAndCallbackStyleMacro, p({
+		writeToPath: 'unexistent/path/to/file.txt',
+		expectFileWasCreated: true
+	}));
+
+	test.cb('writeTo a relative path', writeToPromiseAndCallbackStyleMacro, p({
+		writeToRelativePath: 'relative/path/to/file.txt',
+		expectFileWasCreated: true
+	}));
+
+	test.cb('writeTo a relative path with a custom cwd', writeToPromiseAndCallbackStyleMacro, p({
+		writeToRelativePath: 'relative/path/to/file.txt',
+		expectFileWasCreated: true,
+		writeToDependencies: {
+			cwd: true
+		}
+	}));
+})
+
+/*-------------------------------------------------*/
+/*-------------------------------------------------*/
+/*-------------------------------------------------*/
+
+function writeToDependencyOptionsMacro(t, {
+	write,
+	copy,
+	dependency,
+	defaultOptions,
+	expectedOptions,
+	callbackStyle
+}) {
+	expectedOptions = (defaultOptions ? defaultOptions : expectedOptions)[dependency];
+
+	const expectedFile = '/absolute/file/path.txt';
+	const expectedContent = 'expected file content';
+
+	t.plan(2);
+
+	mockFileWriterOptionsObject({write, copy}, expectedContent, coreTest);
+
+	function coreTest(config, expectedPathToCopy){
+		const FileWriter = requireFromIndex('sources/file-writer');
+		const fileWriter = new FileWriter(Object.assign({}, (defaultOptions ? {} : expectedOptions), config));
+
+		const dependenciesMocks = {
+			fs: {
+				mkdir(dirPath, mode, cb){cb()},
+				stat(destPath, cb){cb()},
+				writeFile(passedFile, passedContent, passedOptions, cb){
+					if (dependency === 'fs.writeFile') {
+						assert.equal(passedFile, expectedFile);
+						assert.equal(passedContent, expectedContent);
+						assert.deepEqual(passedOptions, expectedOptions);
+
+						t.pass();
+					}
+					
+					cb();
+				},
+				createReadStream(passedPathToCopy, passedOptions){
+					let stop = false;
+					return new ReadableStream({
+						read(){
+							if (dependency === 'fs.createReadStream' && !stop) {
+								assert.equal(passedPathToCopy, expectedPathToCopy);
+								assert.deepEqual(passedOptions, expectedOptions);
+
+								t.pass();
+							}
+
+							this.push(stop ? null : expectedContent);
+							stop = true;
+						}
+					});
+				},
+				createWriteStream(passedFile, passedOptions){
+					return new WritableStream({
+						write (chunk, encoding, cb) {
+							const passedContent = chunk.toString('utf-8');
+
+							if (dependency === 'fs.createWriteStream') {
+								assert.equal(passedFile, expectedFile);
+								assert.equal(passedContent, expectedContent);
+								assert.deepEqual(passedOptions, expectedOptions);
+								
+								t.pass();
+							}
+							cb();
+						}
+					});
+				}
+			}
+		};
+
+		if (callbackStyle) {
+			fileWriter.writeTo(expectedFile, ()=>{
+				t.pass();t.end();
+			}, dependenciesMocks);
+		}
+		else{
+			fileWriter.writeTo(expectedFile, null, dependenciesMocks).then(()=>{
+				t.pass();t.end();
+			});
+		}
+	}
+}
+writeToDependencyOptionsMacro.title = (providedTitle, {write, copy, dependency, defaultOptions, callbackStyle}) => `${providedTitle} - .writeTo() using ${write ? 'write' : 'copy'} with a ${write || copy} - dependency ${dependency} ${defaultOptions ? 'default options' : 'options'} - ${callbackStyle ? 'callback' : 'promise'} style`;
+
+writerPossibilities.forEach(function setPossibilityDependencies(possibility){
+	const action = (possibility.write || possibility.copy);
+	const exploded = action.split(' ');
+	const last = exploded[exploded.length - 1];
+
+	if (last === 'string' || last === 'buffer') {
+		possibility.dependencies = ['fs.writeFile'];
+	}
+
+	if (last === 'stream') {
+		possibility.dependencies = ['fs.createWriteStream'];
+	}
+
+	if (last === 'path') {
+		possibility.dependencies = ['fs.createReadStream', 'fs.createWriteStream'];
+	}
+
+	if (last === 'directory') {
+		possibility.dependencies = [];
+	}
+});
+
+writerPossibilities.forEach(({
+	write = null,
+	copy = null,
+	dependencies,
+}) => {
+	assert(Array.isArray(dependencies), `Please provide the dependencies for the possibility "${write || copy}"`);
+
+	dependencies.forEach(dependency => {
+		test.cb(writeToDependencyOptionsMacro, {
+			write,
+			copy,
+			dependency,
+			expectedOptions: {
+				'fs.writeFile': {
+					encoding: 'write-file-encoding'
+				},
+				'fs.createWriteStream': {
+					encoding: 'create-write-stream-encoding'
+				},
+				'fs.createReadStream': {
+					encoding: 'create-read-stream-encoding'
+				}
+			}
+		});
+
+		test.cb(writeToDependencyOptionsMacro, {
+			write,
+			copy,
+			dependency,
+			defaultOptions: {
+				'fs.writeFile': {
+					encoding: 'utf-8'
+				},
+				'fs.createWriteStream': {
+					encoding: 'utf-8'
+				},
+				'fs.createReadStream': {
+					encoding: 'utf-8'
+				}
+			}
+		});
+
+		test.cb(writeToDependencyOptionsMacro, {
+			write,
+			copy,
+			dependency,
+			callbackStyle: true,
+			expectedOptions: {
+				'fs.writeFile': {
+					encoding: 'write-file-encoding'
+				},
+				'fs.createWriteStream': {
+					encoding: 'create-write-stream-encoding'
+				},
+				'fs.createReadStream': {
+					encoding: 'create-read-stream-encoding'
+				}
+			}
+		});
+
+		test.cb(writeToDependencyOptionsMacro, {
+			write,
+			copy,
+			dependency,
+			callbackStyle: true,
+			defaultOptions: {
+				'fs.writeFile': {
+					encoding: 'utf-8'
+				},
+				'fs.createWriteStream': {
+					encoding: 'utf-8'
+				},
+				'fs.createReadStream': {
+					encoding: 'utf-8'
+				}
+			}
+		});
+	});
+});
+
+/*-------------------------------------------------*/
+/*-------------------------------------------------*/
+/*-------------------------------------------------*/
+
+function getThrowingErrorDependency(dep) {
+	const deps = {
+		mkdirp(dir, opts, cb){
+			cb(new Error('mkdirp dependency error'));
+		},
+		isDirectory(dir, cb){
+			cb(new Error('isDirectory dependency error'));
+		}
+	};
+
+	return {
+		[dep]: deps[dep]
+	};
+}
+
+writerPossibilities.forEach(possibility => {
+	const throwingErrorDependencies = possibility.write ? ['mkdirp'] : ['mkdirp', 'isDirectory'];
+
+	function p(...args) {
+		return Object.assign({}, possibility, ...args)
+	}
+
+	throwingErrorDependencies.forEach(dependency => {
+		test.cb(writeToPromiseAndCallbackStyleMacro, p({
+			writeToPath: 'write/to/path/file.txt',
+			writeToDependencies: getThrowingErrorDependency(dependency),
+			expectError: `${dependency} dependency error`,
+			assertAllFilesExist: [{
+				path: 'write/to/path/file.txt',
+				content: false
+			}]
+		}));
+
+		test.cb(writeToPromiseAndCallbackStyleMacro, p({
+			writeToRelativePath: 'write/to/relative/path/file.txt',
+			writeToDependencies: getThrowingErrorDependency(dependency),
+			expectError: `${dependency} dependency error`,
+			assertAllFilesExist: [{
+				path: 'write/to/relative/path/file.txt',
+				content: false,
+				relative: true
+			}]
+		}));
+	});
+});
+
+/*-------------------------------------------------*/
+/*-------------------------------------------------*/
+/*-------------------------------------------------*/
+
 test.cb(writeToPromiseAndCallbackStyleMacro, {
 	write : 'failing promise',
 	writeToPath: 'must-be-preserved.txt',
@@ -218,199 +558,3 @@ test.cb('writeTo a non existent path', writeToPromiseAndCallbackStyleMacro, {
 		content: false
 	}]
 })
-
-/*-------------------------------------------------*/
-/*-------------------------------------------------*/
-/*-------------------------------------------------*/
-
-const writerPossibilities = require('../settings/file-writer.test-config');
-
-test.cb(writeToPromiseAndCallbackStyleMacro, {
-	write: 'string',
-	expectFileWasCreated: true
-})
-
-test.cb('writeTo a non existent path', writeToPromiseAndCallbackStyleMacro, {
-	write: 'string',
-	writeToPath: 'unexistent/path/to/file.txt',
-	expectFileWasCreated: true
-})
-
-test.cb('writeTo a relative path', writeToPromiseAndCallbackStyleMacro, {
-	write: 'string',
-	writeToRelativePath: 'relative/path/to/file.txt',
-	expectFileWasCreated: true
-})
-
-test.cb('writeTo a relative path with a custom cwd', writeToPromiseAndCallbackStyleMacro, {
-	write: 'string',
-	writeToRelativePath: 'relative/path/to/file.txt',
-	expectFileWasCreated: true,
-	writeToDependencies: {
-		cwd: true
-	}
-})
-
-//testWriteToPossibilities(writerPossibilities);
-//testWriteToOptionsPossibilities(writerPossibilities);
-//testDependenciesThrowingAnError(writerPossibilities);
-
-function testWriteToOptionsPossibilities(possibilities){
-	const FileWriter = requireFromIndex('sources/file-writer');
-
-	const expectedDependenciesOptions = {
-		'fs.writeFile': {
-			encoding: 'write-file-encoding'
-		},
-		'fs.createWriteStream': {
-			encoding: 'create-write-stream-encoding'
-		},
-		'fs.createReadStream': {
-			encoding: 'create-read-stream-encoding'
-		}
-	}
-
-	const expectedDependenciesDefaultOptions = {
-		'fs.writeFile': {
-			encoding: 'utf-8'
-		},
-		'fs.createWriteStream': {
-			encoding: 'utf-8'
-		},
-		'fs.createReadStream': {
-			encoding: 'utf-8'
-		}
-	}
-
-	possibilities.forEach(({
-		write = null,
-		copy = null,
-		dependencies,
-	}) => {
-		assert(Array.isArray(dependencies), `Please provide the dependencies for the possibility "${write || copy}"`);
-
-		dependencies.forEach(dependency => {
-			function possibilityOptionsTest(title, {
-				defaultOptions = null
-			} = {}){
-				test.cb(`${title}${defaultOptions ? ' -- default options' : ''}`, t => {
-					const expectedFile = `/absolute/path/${dashify(title)}.txt`;
-					const expectedContent = `${title} -- file content`;
-					const expectedOptions = (defaultOptions ? defaultOptions : expectedDependenciesOptions)[dependency];
-
-					t.plan(2);
-
-					mockFileWriterOptionsObject({write, copy}, expectedContent, (writerOptions, expectedPathToCopy) => {
-						const fileWriter = new FileWriter(Object.assign({}, (defaultOptions ? {} : expectedOptions), writerOptions));
-
-						fileWriter.writeTo(expectedFile, null, {
-							fs: {
-								mkdir(dirPath, mode, cb){cb()},
-								stat(destPath, cb){cb()},
-								writeFile(passedFile, passedContent, passedOptions, cb){
-									if (dependency === 'fs.writeFile') {
-										assert.equal(passedFile, expectedFile);
-										assert.equal(passedContent, expectedContent);
-										assert.deepEqual(passedOptions, expectedOptions);
-
-										t.pass();
-									}
-									
-									cb();
-								},
-								createReadStream(passedPathToCopy, passedOptions){
-									let stop = false;
-									return new ReadableStream({
-										read(){
-											if (dependency === 'fs.createReadStream' && !stop) {
-												assert.equal(passedPathToCopy, expectedPathToCopy);
-												assert.deepEqual(passedOptions, expectedOptions);
-
-												t.pass();
-											}
-
-											this.push(stop ? null : expectedContent);
-											stop = true;
-										}
-									});
-								},
-								createWriteStream(passedFile, passedOptions){
-									return new WritableStream({
-										write (chunk, encoding, cb) {
-											const passedContent = chunk.toString('utf-8');
-
-											if (dependency === 'fs.createWriteStream') {
-												assert.equal(passedFile, expectedFile);
-												assert.equal(passedContent, expectedContent);
-												assert.deepEqual(passedOptions, expectedOptions);
-												
-												t.pass();
-											}
-											cb();
-										}
-									});
-								}
-							}
-						}).then(()=>{
-							t.pass();t.end();
-						});
-					});
-				});
-			}
-
-			const _title = `writeTo options using ${write ? 'write' : 'copy'} with ${write || copy} - ${dependency} options`;
-
-			possibilityOptionsTest(_title);
-			possibilityOptionsTest(_title, {
-				defaultOptions: expectedDependenciesDefaultOptions
-			});
-		});
-	});
-}
-
-function testDependenciesThrowingAnError(possibilities) {
-	possibilities.forEach(({
-		write = null,
-		copy = null
-	}) => {
-		const throwingErrorDependencies = write ? ['mkdirp'] : ['mkdirp', 'isDirectory'];
-
-		function getThrowingErrorDependency(dep) {
-			const deps = {
-				mkdirp(dir, opts, cb){
-					cb(new Error('mkdirp dependency error'));
-				},
-				isDirectory(dir, cb){
-					cb(new Error('isDirectory dependency error'));
-				}
-			};
-
-			return {
-				[dep]: deps[dep]
-			};
-		}
-
-		throwingErrorDependencies.forEach(dep => {
-			function throwingErrorDependenciesTest(title, relative = false){
-				const expectedFile = `${dashify(title)}.txt`;
-				let expectedContent = `${title} -- file content`;
-
-				writeToTestPromiseAndCallbackStyle(title, {
-					writerConfigMockParams: [{ write, copy }, expectedContent],
-					testDirectory: {title: dashify(title), relative},
-					writeTo: expectedFile,
-					writeToDependencies: getThrowingErrorDependency(dep),
-					expectError: `${dep} dependency error`,
-					assertAllFilesExist: [{
-						path: expectedFile,
-						content: false,
-						relative
-					}]
-				});
-			}
-
-			throwingErrorDependenciesTest(`writeTo using ${write ? 'write' : 'copy'} with ${write || copy} and the dependency "${dep}" throwing an error`);
-			throwingErrorDependenciesTest(`writeTo a relative path using ${write ? 'write' : 'copy'} with ${write || copy} and the dependency "${dep}" throwing an error`, /*relative*/ true);
-		});
-	});
-}
