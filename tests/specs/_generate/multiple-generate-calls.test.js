78a13b41382e5efc9426796b8d6b81168888a1ab
Alexis Tessier <alexistessierdev@gmail.com>
2017-11-07 11:01:25 +0100

    update dependencies and add some tests to do

--- tests/specs/_generate/multiple-generate-calls.test.js
+++ tests/specs/_generate/multiple-generate-calls.test.js
@@ -8,4 +8,22 @@ const requireFromIndex = require('../../utils/require-from-index');
 
 /*------------------------*/
 
-test.todo('multiple generate calls');
+test.todo('default configKeysCache option - test multiple generate calls with no conflicts - synchronous');
+test.todo('default configKeysCache option - test multiple generate calls with no conflicts - asynchronous');
+test.todo('default configKeysCache option - test multiple generate calls with conflicts - synchronous');
+test.todo('default configKeysCache option - test multiple generate calls with conflicts - asynchronous');
+
+test.todo('override configKeysCache option using the instance generator - test multiple generate calls with no conflicts - synchronous');
+test.todo('override configKeysCache option using the instance generator - test multiple generate calls with no conflicts - asynchronous');
+test.todo('override configKeysCache option using the instance generator - test multiple generate calls with conflicts - synchronous');
+test.todo('override configKeysCache option using the instance generator - test multiple generate calls with conflicts - asynchronous');
+
+test.todo('override configKeysCache option using the generate function - test multiple generate calls with no conflicts - synchronous');
+test.todo('override configKeysCache option using the generate function - test multiple generate calls with no conflicts - asynchronous');
+test.todo('override configKeysCache option using the generate function - test multiple generate calls with conflicts - synchronous');
+test.todo('override configKeysCache option using the generate function - test multiple generate calls with conflicts - asynchronous');
+
+test.todo('override configKeysCache option using the generate function after using the instance generator - test multiple generate calls with no conflicts - synchronous');
+test.todo('override configKeysCache option using the generate function after using the instance generator - test multiple generate calls with no conflicts - asynchronous');
+test.todo('override configKeysCache option using the generate function after using the instance generator - test multiple generate calls with conflicts - synchronous');
+test.todo('override configKeysCache option using the generate function after using the instance generator - test multiple generate calls with conflicts - asynchronous');
